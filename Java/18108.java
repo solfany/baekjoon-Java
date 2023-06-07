64cf05c7cde96af76c530c8db4ab674a3d3bdf5c
solfany <solfany1999@gmail.com>
2023-06-08 01:22:06 +0900

    Create 18108.java
    백준 18108

---
+++ Java/18108.java
@@ -0,0 +1,16 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class N18108 {
+
+	public static void main(String[] args) {
+		int y;
+		Scanner sc = new Scanner(System.in);
+		y = sc.nextInt();
+		System.out.println(y - 543 );
+	}
+}
+
+
+// 주어진 예제에서는 불교력의 연도를 서력 연도에서 빼고 그 값을 다시 y에서 빼도록 연산을 수행했다.
