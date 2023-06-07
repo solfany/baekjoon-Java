c0a7396590b6cce48dba00eb181cfed3db4a26ef
solfany <solfany1999@gmail.com>
2023-06-07 23:55:13 +0900

    Create 10926.java
    10926

---
+++ Java/Silver/10926.java
@@ -0,0 +1,26 @@
+2023 WED 07, made by Solfany
+
+package backjoon;
+
+import java.util.Scanner;
+
+public class N10926 {
+	public static void main(String[] args) {
+		String name;
+		
+		Scanner sc = new Scanner(System.in);
+		name = sc.next();
+	System.out.println(name+"??!");
+	}
+}
+
+
+
+// 처음엔 입력 예제에 나와있는 값을 입력해야 뒤에 “??!” 기호가 붙는줄 알고
+// if 문을 사용해… 작성했다..  그런데 오답이라 나와서  
+// 다른 방식으로 접근하기로 하고 입력 값이 해당 기호만 붙여 출력되도록 수정한 후 제출하니 맞았다고 나왔다. 
+
+// 해당 코드를 풀이 하면서 문제 해결 능력 뿐만 아니라 
+// 문제를 읽고 이해하는 부분도 상당히 중요하다는 걸 
+// 다시 한번 알게 되어서 다행이다… ! 하하  
+
