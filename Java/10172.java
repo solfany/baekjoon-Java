162778a1a36e9e95a77a2506bed7a034e431ff64
solfany <solfany1999@gmail.com>
2023-06-12 02:06:56 +0900

    Create 10172.java
    개 출력

---
+++ Java/10172.java
@@ -0,0 +1,12 @@
+package joon01;
+public class N10172 {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
+
+// 내가 아는 강아지 일리 없어..
