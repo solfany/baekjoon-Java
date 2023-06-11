faec65aaac2f63e0c65da9d58feee4bdb64b586e
solfany <solfany1999@gmail.com>
2023-06-12 02:05:56 +0900

    Update 10171.java
    고양이 출력..

--- Java/10171.java
+++ Java/10171.java
@@ -1,10 +1,11 @@
 package joon01;
-public class N10172 {
+public class N10171 {
 	public static void main(String[] args) {
-		System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 }
+
+//고양이인지는 모르겠지만.. 고양이...
