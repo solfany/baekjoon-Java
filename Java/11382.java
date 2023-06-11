3f2a3a9bd3d416f6a9a23d52592e526dba47f232
solfany <solfany1999@gmail.com>
2023-06-12 01:47:00 +0900

    Create 11382.java
    11382

---
+++ Java/11382.java
@@ -0,0 +1,12 @@
+package joon01;
+
+import java.util.Scanner;
+
+public class N11382 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print(sc.nextLong() + sc.nextLong() + sc.nextLong());
+		sc.close();
+	}
+}
