27048fd2581141c5f3af0176ba64a307441dd0a8
solfany <solfany1999@gmail.com>
2023-06-29 18:27:22 +0900

    백준 문제 업로드

---
+++ Java/2739.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		for(int Y = 1; Y < 10; Y++  ) {
+			System.out.println(N + "*" +  Y +"= " +(N*Y));
+		}
+	}
+}
