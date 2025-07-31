e831c3620af4c643b7b53e63c18c07a3fd32f31c
solfany <solfany1999@gmail.com>
2025-07-31 09:24:45 +0900

    hotfix: 2438번 문제 class 이름 수정 및 파일 이름 변경

---
+++ Java/N2438.java
@@ -0,0 +1,19 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class N2438 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); //찍을  갯수
+        int N;
+        N = s.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int o = 1; o <= i; o++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
